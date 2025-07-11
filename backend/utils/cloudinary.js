fb77f6b14ae1a65cb42ab233b9dabcaab1d059c9
Akash190iiit <admin@Akash-BhardwajTech.local>
2025-07-12 01:46:52 +0530

    fixing file upload

--- backend/utils/cloudinary.js
+++ backend/utils/cloudinary.js
@@ -4,15 +4,15 @@ const { CloudinaryStorage } = require("multer-storage-cloudinary");
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 const storage = new CloudinaryStorage({
   cloudinary,
   params: {
     folder: "vistagram",
-    allowed_formats: ["jpg", "png", "jpeg"]
-  }
+    allowed_formats: ["jpg", "png", "jpeg"],
+  },
 });
 
 module.exports = { cloudinary, storage };

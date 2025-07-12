9ccaee89922bcbee5129eb30d92304280cb76c5c
Akash190iiit <admin@Akash-BhardwajTech.local>
2025-07-12 22:23:37 +0530

    cores

--- backend/server.js
+++ backend/server.js
@@ -10,11 +10,15 @@ dotenv.config();
 
 const app = express();
 // app.use(cors());
+// app.use(cors({
+//     origin: [
+//       "https://vistagram.vercel.app",      // 🔁 Replace with your actual Vercel domain
+//       "http://localhost:3000"              // ✅ Optional: for local frontend dev
+//     ],
+//     credentials: true,
+//   }));
 app.use(cors({
-    origin: [
-      "https://vistagram.vercel.app",      // 🔁 Replace with your actual Vercel domain
-      "http://localhost:3000"              // ✅ Optional: for local frontend dev
-    ],
+    origin: true,
     credentials: true,
   }));
 app.use(express.json()); // for parsing JSON
